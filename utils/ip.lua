0d333766cf519ed0ff8a7e8da35db0a220e9c59d
lzj <lzj@mac-mini.local>
2025-03-11 22:51:39 +0800

    update

--- utils/ip.lua
+++ utils/ip.lua
@@ -8,32 +8,31 @@ ip.local_ip = function()
     -- 获取本地 IP 地址（Linux/macOS）
     local local_ip = "unknown"
     if platform.is_mac then
-    -- macOS 使用 ipconfig 命令获取 IP 地址
-    local success, stdout, stderr = wezterm.run_child_process { "bash", "-c", "ipconfig getifaddr en0" }
-    local_ip = stdout:match("%S+")
+        -- macOS 使用 ipconfig 命令获取 IP 地址
+        local success, stdout, stderr = wezterm.run_child_process { "bash", "-c", [[ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}']] }
+        local_ip = success and stdout:match("%S+") or local_ip
 
     elseif platform.is_linux then
-    -- Linux 使用 hostname 命令获取 IP 地址
-    local success, stdout, stderr = wezterm.run_child_process { "bash", "-c", "hostname -I" }
-    local_ip = stdout:match("%S+")
+        -- Linux 使用 hostname 命令获取 IP 地址
+        local success, stdout, stderr = wezterm.run_child_process { "bash", "-c", "hostname -I" }
+        local_ip = success and stdout:match("%S+") or local_ip
 
-    elseif platform.is_windows then
-    -- Windows 使用 ipconfig 获取 IP 地址
-    local success, stdout, stderr = wezterm.run_child_process { "cmd", "/c", "ipconfig" }
-    local_ip = stdout:match("IPv4 地址[ .]+: ([%d%.]+)")
+    elseif platform.is_win then
+        -- Windows 使用 ipconfig 获取 IP 地址
+        local success, stdout, stderr = wezterm.run_child_process { "cmd", "/c", "ipconfig" }
+        local_ip = success and stdout:match("IPv4 地址[ .]+: ([%d%.]+)") or local_ip
     end
     return local_ip
 
 end
 
 ip.public_ip = function()
-    local success, stdout, stderr = wezterm.run_child_process { "curl", "-s", "https://api64.ipify.org" }
+    -- local success, stdout, stderr = wezterm.run_child_process { "curl", "-s",  "https://api64.ipify.org" }
     -- local success, stdout, stderr = wezterm.run_child_process { "curl", "-s", "https://realip.cc/simple" }
-    local public_ip=stdout
+    local success, stdout, stderr = wezterm.run_child_process { "curl", "-s", "https://4.ipw.cn" }
 
-    if not public_ip then
-    public_ip = 'N/A'
-    end
+    local public_ip = success and stdout:match("%S+") or "N/A"
+
     return public_ip
 end
 
